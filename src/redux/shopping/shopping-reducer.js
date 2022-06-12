a7ea512069a3ab009484b30b0cb93ddd21071702
kiruicodex <55911769+victorkirui@users.noreply.github.com>
2022-06-12 12:48:18 +0300

    Update shopping-reducer.js

--- src/redux/shopping/shopping-reducer.js
+++ src/redux/shopping/shopping-reducer.js
@@ -46,7 +46,7 @@ const shopReducer = (state = INITIAL_STATE, action) => {
                 ? { ...item, qty: item.qty + 1 }
                 : item
             )
-          : [...state.cart, { ...item, qty: 1 }],
+          : [...state.cart, { ...item, qty: 1, attributes:action.payload.attributeValues}],
       };
 
     case actionTypes.REMOVE_FROM_CART:
